ce178f0588ed5bdd16251d137bf4958b332fa6ec
guoyu <guoyuzha@cisco.com>
2020-11-16 18:56:56 +0800

    feat: remove function parameter data type

--- src/com/github/bootcanal/GitHub.groovy
+++ src/com/github/bootcanal/GitHub.groovy
@@ -4,8 +4,8 @@ class GitHub {
     def String commitId() {
         return sh(script: "git rev-parse HEAD", returnStdout: true)
     }
-    def checkPR(String token, String organization, String repo, String commit_id, String state) {
-        def String _context = 'continuous-integration/jenkins/unit-test'
+    def checkPR(token, organization, repo, commit_id, state) {
+        def _context = 'continuous-integration/jenkins/unit-test'
         def pr_url = "https://api.github.com/repos/${organization}/${repo}/statuses/${commit_id}"
         def payload = JsonOutput.toJson([context: ${_context}, state: ${state}, target_url: 'https://github.com/bootcanal/canal', description: ${state}])
         sh("curl -XPOST -H \"Authorization: token ${token}\" ${pr_url} -d ${payload}")
