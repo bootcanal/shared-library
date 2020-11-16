8b8ef2225d75d44b1ba98284a9eb212c763c0060
guoyu <guoyuzha@cisco.com>
2020-11-16 18:32:04 +0800

    fix: bug fix

--- src/com/github/bootcanal/GitHub.groovy
+++ src/com/github/bootcanal/GitHub.groovy
@@ -4,10 +4,10 @@ class GitHub {
     def String commitId() {
         return sh(script: "git rev-parse HEAD", returnStdout: true)
     }
-    def checkPR(String token, String organization, String repo, String commit_id, String state) {
+    def checkPR(Closure token, String organization, String repo, String commit_id, String state) {
         def String _context = 'continuous-integration/jenkins/unit-test'
         def pr_url = "https://api.github.com/repos/${organization}/${repo}/statuses/${commit_id}"
         def payload = JsonOutput.toJson([context: ${_context}, state: ${state}, target_url: 'https://github.com/bootcanal/canal', description: ${state}])
-        sh("curl -XPOST -H \"Authorization: token ${token}\" ${pr_url} -d ${payload}")
+        sh("curl -XPOST -H \"Authorization: token ${token()}\" ${pr_url} -d ${payload}")
     }
 }
