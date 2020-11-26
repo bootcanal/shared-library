771b04f14c5fc85553b2b1a928625f29a4f819be
guoyu <guoyu07@gmail.com>
2020-11-26 19:58:46 +0800

    add static method

--- src/com/github/bootcanal/GitHub.groovy
+++ src/com/github/bootcanal/GitHub.groovy
@@ -76,7 +76,7 @@ class GitHub implements Serializable {
         return state
     }
 
-    def checkPR(token, organization, repo, commit_id, state) {
+    def static checkPR(token, organization, repo, commit_id, state) {
         def _context = 'continuous-integration/jenkins/unit-test'
         def pr_url = "https://api.github.com/repos/${organization}/${repo}/statuses/${commit_id}"
         def payload = JsonOutput.toJson([context: ${_context}, state: ${state}, target_url: 'https://github.com/bootcanal/canal', description: ${state}])
