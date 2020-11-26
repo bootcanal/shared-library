30eb523b2433f95355f47d9d7b0b5e5d9283e96b
guoyu <guoyu07@gmail.com>
2020-11-26 20:11:24 +0800

    bugfix

--- src/com/github/bootcanal/GitHub.groovy
+++ src/com/github/bootcanal/GitHub.groovy
@@ -78,7 +78,7 @@ class GitHub implements Serializable {
     def static checkPR(token, organization, repo, commit_id, state) {
         def _context = 'continuous-integration/jenkins/unit-test'
         def pr_url = "https://api.github.com/repos/${organization}/${repo}/statuses/${commit_id}"
-        def payload = JsonOutput.toJson([context: ${_context}, state: ${state}, target_url: 'https://github.com/bootcanal/canal', description: ${state}])
+        def payload = JsonOutput.toJson([context: _context, state: state, target_url: 'https://github.com/bootcanal/canal', description: state])
         sh("curl -XPOST -H \"Authorization: token ${token}\" ${pr_url} -d ${payload}")
     }
 }
